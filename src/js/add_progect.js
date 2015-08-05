f51ba22b8c0790e4135f226fff170c063b77fe03
MegaDance <megadance2000@gmail.com>
2015-08-05 11:35:03 +0300

    Admin.css

--- src/js/add_progect.js
+++ src/js/add_progect.js
@@ -2,23 +2,47 @@ var myModule = (function () {
 
     // Инициализирует наш модуль
     var init = function () {
-                _setUpListners();
-            };
+        _setUpListners();
+    };
 
     // Прослушивает события 
     var _setUpListners = function () {
         $('#add-new-item').on('click', _showModal); //открыть попап окно
-        $('add-')
-            };
-
-    var _showModal = function  (ev) {
-        console.log ('Вызов модального окна');
-        ev.preventDefault();
-        $('#new-progect-popup').bPopup();
-            };
-
-    return {
-        init: init
+        $('#add-new-project').on('submit', _addProgect); //добавим проект
     };
+
+    var _showModal = function (ev) {
+        console.log('Вызов попап окна');
+        ev.preventDefault();
+        $('#new-progect-popup').bPopup({
+            speed: 500,
+            transition: 'slideIn'
+        });
+    };
+    var _addProgect = function (ev) {
+        console.log('Добавили проект');
+        ev.preventDefault();
+
+        //обьявляем переменные
+        var form = $(this),
+            url = 'add_prog.php',
+            data = form.serialize();
+        console.log(data);
+
+        //Ajax запросы на сервер
+        $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json',
+            data: data,
+        })
+        .done(function(ans) {
+            console.log('Success');
+            console.log(ans);
+        })
+    };
+return {
+    init: init
+};
 })();
-    myModule.init(); 
+myModule.init();
