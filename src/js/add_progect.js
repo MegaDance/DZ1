8276add7cede9ab2c2b6980e4ae2e635a6d2cc37
MegaDance <megadance2000@gmail.com>
2015-08-06 16:12:36 +0300

    IE8

--- src/js/add_progect.js
+++ src/js/add_progect.js
@@ -1,3 +1,15 @@
+var alertFallback = true;
+   if (typeof console === "undefined" || typeof console.log === "undefined") {
+     console = {};
+     if (alertFallback) {
+         console.log = function(msg) {
+              alert(msg);
+         };
+     } else {
+         console.log = function() {};
+     }
+   }
+
 var myModule = (function () {
 
     // Инициализирует наш модуль
